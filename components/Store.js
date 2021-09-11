f931abd9dcc28f7a91137fd8aaf29048fc31b874
qxl0 <qxl0@yahoo.com>
2021-09-10 20:15:46 -0400

    cleaned up

--- components/Store.js
+++ components/Store.js
@@ -15,21 +15,6 @@ export default class Store extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      data: [
-        {id:1, title: "Product 1",  price:"$ 25.00 USD", image:"https://via.placeholder.com/400x200/FFB6C1/000000"},
-        {id:2, title: "Product 2",  price:"$ 10.13 USD", image:"https://via.placeholder.com/400x200/FA8072/000000"} ,
-        {id:3, title: "Product 3",  price:"$ 12.12 USD", image:"https://via.placeholder.com/400x200/87CEEB/000000"}, 
-        {id:4, title: "Product 4",  price:"$ 11.00 USD", image:"https://via.placeholder.com/400x200/4682B4/000000"}, 
-        {id:5, title: "Product 5",  price:"$ 20.00 USD", image:"https://via.placeholder.com/400x200/008080/000000"}, 
-        {id:6, title: "Product 6",  price:"$ 33.00 USD", image:"https://via.placeholder.com/400x200/40E0D0/000000"}, 
-        {id:7, title: "Product 7",  price:"$ 20.95 USD", image:"https://via.placeholder.com/400x200/EE82EE/000000"}, 
-        {id:8, title: "Product 8",  price:"$ 13.60 USD", image:"https://via.placeholder.com/400x200/48D1CC/000000"},
-        {id:9, title: "Product 9",  price:"$ 15.30 USD", image:"https://via.placeholder.com/400x200/191970/000000"},
-        {id:9, title: "Product 10", price:"$ 21.30 USD", image:"https://via.placeholder.com/400x200/7B68EE/000000"},
-      ]
-    };
-  }
 
   addProductToCart = () => {
     Alert.alert('Success', 'The product has been added to your cart')
