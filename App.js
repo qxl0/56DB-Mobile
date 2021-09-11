5fb23e4f77fbde9f3502f7da262d1090bd1bfbcd
qxl0 <qxl0@yahoo.com>
2021-09-10 20:04:47 -0400

    initial check in, itemlist loaded and displayed

--- App.js
+++ App.js
@@ -1,13 +1,69 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import Store from './components/Store';
+import { ApolloProvider, Query } from 'react-apollo';
+import ApolloClient from 'apollo-boost';
+import gql from 'graphql-tag';
+import { AppContext } from './AppContext';
+
+const client= new ApolloClient({
+  uri: 'http://192.168.1.48:4000/graphql'
+});
+
+const getQuery = () => {
+  const desc = "abb";
+  return
+  `
+    query {
+          queryitems(desc: "abb"}){
+            ID,
+            Description,
+            Price
+          }
+        }
+    `
+};
 
 export default function App() {
+
+  const [state, setState] = useState({query: null});
+
+  useEffect(() => {
+    console.log("useEffect:", state.query);
+    const query = getQuery();
+    setState({query});
+  }, [ state.query] );
+
+  if (state.query === null) {
+    console.log("state.query === null");
+    return <View style={styles.loading}></View>  
+  }
+
+  console.log("state.query !== null", state.query);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+    <ApolloProvider client={client}>
+      {/* <Query query={gql `${state.query}` }> */}
+      <Query query={
+            gql `query { queryitems(desc: "abb") { ID, Description,Price}}` } >
+        {({ loading, error, data }) => {
+          if (loading || error) return <ActivityIndicator size="large" color="#0000ff" />
+          const items = data.queryitems;
+          console.log("items: ", items);
+          const items2 = items.map(item => 
+            { return {...item, Price: `$ ${item.Price} USD`}} );
+          return (
+            <AppContext.Provider value={ items2 }>
+              <View style={styles.container}>
+                <Store />
+                <StatusBar style="auto" />
+              </View>
+            </AppContext.Provider>
+          )
+        }}
+      </Query>
+    </ApolloProvider>
   );
 }
 
@@ -18,4 +74,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  loading: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
